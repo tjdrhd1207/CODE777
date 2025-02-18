673f937adf80c5a74949c8e3532bb0a61887542d
kimjaemin <jaeminkim@hansol.com>
2025-02-18 13:50:56 +0900

    main 소스 변경

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 import Player from "./player.js";
 import Game from "./game.js";
 
+const btn = document.querySelector(".shuffle-button");
+
 const player1 = new Player("나", 0);
 const player2 = new Player("용준", 1);
 const player3 = new Player("동준", 2);
 const player4 = new Player("호용", 3);
 
-const game = new Game([player1, player2, player3, player4]);
 
-game.start();
+btn.addEventListener("click", () => {
+    const cardDeck = document.querySelector('.card-deck');
+    cardDeck.classList.add('card-shuffle');
+
+    const game = new Game([player1, player2, player3, player4]);
+    game.start();
+});
+
