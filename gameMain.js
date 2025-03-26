57faf3192f8cceb0d0404a837ce731dd699a44ea
whlthd1207 <whlthd1207@naver.com>
2025-03-26 23:07:36 +0900

    [v0.0.26] - 메인페이지 효과음 넣기 - 리스트 호버, 클릭 이벤트트

---
+++ gameMain.js
@@ -0,0 +1,13 @@
+const hoverSound = new Audio('assets/mouse-hover.mp3');
+
+let userInteracted = false;  // 사용자의 상호작용 여부
+
+const mainList = document.querySelectorAll('.main-list');
+
+mainList.forEach((list) => {
+    list.addEventListener('click', () => {
+        hoverSound.currentTime = -1000;
+        hoverSound.play().catch(error => console.error('Sound play error', error));
+        list.classList.add('clicked-list');
+    });
+});
