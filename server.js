5286f78471e4dced94c4f4d34c6dc9c14a507eb0
kimjaemin <jaeminkim@hansol.com>
2025-04-08 08:48:39 +0900

    [v0.0.32] 백엔드 몽고db연동 성공 - app.use(express.json()) 미들웨어 추가가

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const { MongoClient } = require("mongodb");
 const app = express();
 const port = 3000;
 
-const uri = "mongodb+srv://jaemin:sjk%40931207@cluster0.3lo3bxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://jaemin:hansol@cluster0.3lo3bxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 app.use(cors({
     origin: "http://127.0.0.1:5500"
 }));
+app.use(express.json());  // JSON 파싱 미들웨어
 
 app.post("/data", async (req, res) => {
     await client.connect();
     const db = client.db("mydb");
     const data = db.collection("user").find().toArray();
-    console.log("받은 데이터 : " + req.body);
+    console.log("받은 데이터 : ");
+    console.log(req.body);
     res.json(data);
 });
 
