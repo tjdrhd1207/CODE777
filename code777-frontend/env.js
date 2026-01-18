b103e6de731367f203bf52c320e56581bc3a8ac7
jaemin <whlthd1207@naver.com>
2026-01-18 18:09:53 +0900

    front env 수정

--- code777-frontend/env.js
+++ code777-frontend/env.js
@@ -1,6 +1,8 @@
 // 추후 .env로 관리
 // npx serve에서는 .env를 자동으로 못일기 때문에 js로 표현 (현재 VITE 같은 빌드도구 안쓰기 때문에)
 
+const isLocalhost = location.hostname === "localhost" || location.hostnae === "127.0.0.1";
+
 export const ENV = {
-    BACKEND_URL: "http://localhost:4000"
+    BACKEND_URL: isLocalhost ? "http://localhost:4000" : "https://code777.fly.dev"
 };
